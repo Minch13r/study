4d124324f3ff937277324202857bdb9f9a9715b9
Minch13r <0414minyoung@naver.com>
2024-12-30 10:32:40 +0900

    2중for문(구구단)

---
+++ src/day003/class02/Test01.java
@@ -0,0 +1,14 @@
+package day003.class02;
+
+// 2중 for문
+public class Test01 {
+    public static void main(String[] args) {
+        // 구구단
+        for(int a=2;a<=9;a++){
+            for(int i=1;i<=9;i++){
+                System.out.println(a+" x "+i+" = "+(a*i));
+            }
+            System.out.println("-----------");
+        }
+    }
+}

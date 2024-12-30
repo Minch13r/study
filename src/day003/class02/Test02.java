2ed38a2ea2a4e0bed09a8fd2a30648f63d718a1f
Minch13r <0414minyoung@naver.com>
2024-12-30 10:38:23 +0900

    2중for문(별찍기)

---
+++ src/day003/class02/Test02.java
@@ -0,0 +1,12 @@
+package day003.class02;
+
+public class Test02 {
+    public static void main(String[] args) {
+        for(int a=0;a<5;a++){
+            for(int i=0;i<=5;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

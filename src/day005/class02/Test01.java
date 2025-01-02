c3a0480caa8c873ae1185130599bea65a0a0178d
Minch13r <0414minyoung@naver.com>
2025-01-02 12:32:44 +0900

    배열 사용자 입력값 저장

---
+++ src/day005/class02/Test01.java
@@ -0,0 +1,30 @@
+package day005.class02;
+
+import java.util.Scanner;
+
+public class Test01 {
+    public static void main(String[] args) {
+        // 사용자가 입력하는 값들을 저장해보기
+        int[] datas = new int[3];
+        Scanner sc = new Scanner(System.in);
+
+        // datas.length
+        // 길이, 사이즈, 요소의 개수
+        for(int i=0; i<datas.length; i++){
+            while(true){
+                System.out.print((i+1) + "번째 정수 입력 >> ");
+                datas[i]=sc.nextInt();
+
+                if(datas[i]>0){ //종료조건
+                    break;
+                }
+                System.out.print("양수만 입력해주세요!");
+            }
+        }
+        System.out.print("[ ");
+        for(int v:datas){
+            System.out.print(v + " ");
+        }
+        System.out.print("]");
+    }
+}

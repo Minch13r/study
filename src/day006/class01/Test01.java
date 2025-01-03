4063c3a17fe81e4c1ab665dad7c394510c0f368d
Minch13r <0414minyoung@naver.com>
2025-01-03 11:07:33 +0900

    복습

---
+++ src/day006/class01/Test01.java
@@ -0,0 +1,14 @@
+package day006.class01;
+//복습
+public class Test01 {
+    public static void main(String[] args) {
+        int [] datas = new int[3];
+        //3가지 조건
+        for(int i=0;i<3;i++){
+            datas[i]=(i+1)*10;
+        }
+        for(int v : datas){
+            System.out.print(v + " ");
+        }
+    }
+}

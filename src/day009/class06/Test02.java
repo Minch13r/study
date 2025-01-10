fa92348a22959e759c0062fe12f5231f5230ce4e
Minch13r <0414minyoung@naver.com>
2025-01-10 16:21:59 +0900

    작자미상

---
+++ src/day009/class06/Test02.java
@@ -0,0 +1,28 @@
+package day009.class06;
+/*     2) 책 객체들을 저장하고 싶습니다.
+	   책이란, 제목과 작가로 구성되어있습니다.
+	   만약, 작가를 알 수 없는 책이라면 작가의 이름을 작자미상으로 설정해주세요
+*/
+class Book { // 클래스
+    String title; // 제목
+    String writer; // 작가
+    Book(String title){ // 작자미상 오버로딩
+        this.title = title;
+        this.writer = "작자미상";
+    }
+
+    Book(String title, String writer){ // 기본 생성자
+        this.title = title;
+        this.writer = writer;
+    }
+}
+
+public class Test02 {
+    public static void main(String[] args) {
+        Book book01 = new Book("해리포터", "JK톨링");
+        Book book02 = new Book("춘향전");
+
+        System.out.println(book01.title+" "+book01.writer);
+        System.out.println(book02.title+" "+book02.writer);
+    }
+}

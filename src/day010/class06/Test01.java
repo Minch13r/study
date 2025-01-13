a3ed7e6898d9f31482e2b6e7f7d24b9cb446855e
Minch13r <0414minyoung@naver.com>
2025-01-13 17:24:28 +0900

    implicit

--- src/day010/class06/Test01.java
+++ src/day010/class06/Test01.java
@@ -6,37 +6,37 @@ import java.util.Scanner;
 class Student {
     static int studentNum=1001;
     int num; // PK 값은 사용자가 부여 xxx
-    String name;
-    int score;
-    char grade;
+    String name; // 이름
+    int score; // 점수
+    char grade; // 학점
     Student(String name){
         this(name,0);
     }
     Student(String name,int score){
-        this.num=Student.studentNum++;
+        this.num=Student.studentNum++; // 번호가 1001부터 순차적으로 증가
         this.name=name;
         this.score=score;
-        this.setGrade();
+        this.setGrade(); // 학점은 점수에 따라 측정됨
         System.out.println("학생 정보 생성 완료!");
     }
-    void printInfo() {
+    void printInfo() { // 정보 출력 함수
         System.out.println("번호 : "+this.num);
         System.out.println("이름 : "+this.name);
         System.out.println("점수 : "+this.score);
         System.out.println("등급 : "+this.grade);
     }
-    void changeScore(int score) {
-        this.score=score;
-        this.setGrade();
+    void changeScore(int score) { // 점수 변경
+        this.score=score; // score로 입력된 정수가 this.score을 통해 다시 배정
+        this.setGrade(); // 점수 -> 학점 배정
         System.out.println("학생 정보 변경 완료!");
     }
     void setGrade() {
-        this.grade='C';
+        this.grade='C'; // 원래 C
         if(60<=this.score && this.score<80) {
-            this.grade='B';
+            this.grade='B'; // 60~79 => B
         }
         else if(80<=this.score) {
-            this.grade='A';
+            this.grade='A'; // 80 이상 A
         }
     }
 }
@@ -44,22 +44,22 @@ class Student {
 public class Test01 {
     // '학생(객체)'와 무관한 기능 --->> 함수
     public static boolean isFull(int cnt,Student[] datas) {
-        if( cnt>=datas.length ) {
+        if( cnt >= datas.length ) { // 학생수가 배열의 크기보다 크면 에러처리
             return true;
         }
-        return false;
+        return false; // 아닌경우 저장 가능하게 false 반환
     }
     // 성적 입력 유효성 검사 --->> 함수 ===>> VIEW
 
     public static void main(String[] args) {
 
-        Student[] datas=new Student[3];
+        Student[] datas=new Student[3]; // 배열 크기 3
 
         datas[0]=new Student("티모");
         datas[1]=new Student("모르가나",68);
         // 샘플 데이터
 
-        int cnt=2;
+        int cnt=2; // 현재 학생수 2명
 
         Scanner sc=new Scanner(System.in);
         while(true) {
@@ -77,12 +77,14 @@ public class Test01 {
             if(menu==0) {
                 break;
             }
-            else if(menu==1) {
+
+            else if(menu==1) { // 전체출력
                 for(int i=0;i<cnt;i++) {
                     datas[i].printInfo();
                 }
             }
-            else if(menu==2) {
+
+            else if(menu==2) { // 번호로 학생 검색
                 System.out.print("번호입력 >> ");
                 int num=sc.nextInt();
 
@@ -103,7 +105,7 @@ public class Test01 {
                     System.out.println("해당 학생은 없습니다...");
                 }
             }
-            else if(menu==3) {
+            else if(menu==3) { // 학생추가
                 if(isFull(cnt,datas)) {
                     System.out.println("학생부 데이터에 저장공간이 없습니다!");
                     continue;
@@ -122,7 +124,8 @@ public class Test01 {
                     datas[cnt++]=new Student(name);
                 }
             }
-            else if(menu==4) {
+
+            else if(menu==4) { // 성적변경
                 System.out.print("번호입력 >> ");
                 int num=sc.nextInt();
 

95177ba9ab6cd348d3d69c8e1dabc01913c7c907
Minch13r <0414minyoung@naver.com>
2025-01-20 11:01:06 +0900

    exception

--- src/day014/class02/Test01.java
+++ src/day014/class02/Test01.java
@@ -1,11 +1,25 @@
 package day014.class02;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // 정수를 1개 입력해주세요
 public class Test01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println();
+        int num;
+        while(true) {
+            System.out.print("정수입력 >> ");
+            try {
+                num = sc.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("다시 입력!");
+                //버퍼 비우기
+                sc.nextLine();
+            }
+        }
+        System.out.println("num = " + num);
+
     }
 }
---
+++ src/day014/class02/Test04.java
@@ -0,0 +1,31 @@
+package day014.class02;
+
+import java.util.ArrayList;
+
+// [ 컬렉션 프레임워크 : list ]
+public class Test04 {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        // 배열 3요소 중에 관련이 있는지만 해당하면 넣을 수 있음
+        // 자료형 상관없이 다 가능
+        al.add(10);
+        al.add(0,11);
+        al.add(20);
+        al.add(30);
+        // al.clear();
+        System.out.println(al.contains(31));
+        System.out.println(al.get(1));
+        System.out.println(al.isEmpty());
+        System.out.println(al.size());
+        al.set(0, 123);
+        al.remove(0);
+        System.out.println(al);
+
+
+        ArrayList<Integer> datas = new ArrayList<>();
+        // <> 제네릭
+        // : 저장할 데이터의 타입을 '강제'
+        datas.add(10);
+        // Pokemon 이런것도 들어갈 수 있음 <Pokemon> 이런식으로
+    }
+}

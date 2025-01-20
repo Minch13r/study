c44ec12409de7a0bac50e324e20fbfe512feffe6
Minch13r <0414minyoung@naver.com>
2025-01-20 15:29:02 +0900

    generic

---
+++ src/day014/class06/Test02.java
@@ -0,0 +1,21 @@
+package day014.class06;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Test02 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        Random rand = new Random();
+        while(true){
+            int randNum =rand.nextInt(10);
+            set.add(randNum);
+            if(set.size()>=5){
+                break;
+            }
+        }
+        System.out.println(set);
+    }
+}

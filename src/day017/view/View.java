0b44c7cc09afffe2241bb4fb66481fa8a1e800e1
Minch13r <0414minyoung@naver.com>
2025-01-23 11:46:21 +0900

    쇼핑몰 MVC

--- src/day017/view/View.java
+++ src/day017/view/View.java
@@ -7,64 +7,50 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class View {
-    Scanner sc;
-    public View(){
-        sc = new Scanner(System.in);
+    Scanner scanner;
+    public View() {
+        scanner=new Scanner(System.in);
     }
 
-    public void printDatas(ArrayList<ProductDTO> datas){
-        Iterator itr = datas.iterator();
-        while(itr.hasNext()){
+    public void printDatas(ArrayList<ProductDTO> datas) {
+        Iterator itr=datas.iterator();
+        while(itr.hasNext()) {
             System.out.println(itr.next());
         }
     }
-
-    public void menuInfo(){
-        System.out.println("1. 제품추가");
-        System.out.println("2. 제품 전체 출력");
-        System.out.println("3. 제품 구매");
-        System.out.println("4. 제품 삭제");
-    }
-
     public void printData(ProductDTO data) {
         System.out.println(data);
     }
 
-    public int inputCommand(){
-        System.out.print("메뉴번호 입력 >> ");
-        return sc.nextInt();
-    }
-
-    public void printTrue(){
+    public void printTrue() {
         System.out.println("성공!");
     }
-
-    public void printFalse(){
-        System.out.println("실패");
+    public void printFalse() {
+        System.out.println("실패...");
     }
 
-    public String inputName(){
-        System.out.print("제품명 입력 >> ");
-        return sc.next();
+    public int inputCommand() {
+        System.out.print("메뉴번호입력 >> ");
+        return scanner.nextInt();
     }
-
-    public int inputPrice(){
-        System.out.print("가격 입력 >> ");
-        return sc.nextInt();
+    public String inputName() {
+        System.out.print("제품명입력 >> ");
+        return scanner.next();
     }
-
-    public int inputStock(){
-        System.out.print("재고 입력 >> ");
-        return sc.nextInt();
+    public int inputPrice() {
+        System.out.print("가격입력 >> ");
+        return scanner.nextInt();
     }
-
-    public int inputNum(){
-        System.out.print("번호 입력 >> ");
-        return sc.nextInt();
+    public int inputStock() {
+        System.out.print("재고입력 >> ");
+        return scanner.nextInt();
     }
-
-    public int inputCount(){
-        System.out.print("개수 입력 >> ");
-        return sc.nextInt();
+    public int inputNum() {
+        System.out.print("번호입력 >> ");
+        return scanner.nextInt();
+    }
+    public int inputCount() {
+        System.out.print("구매수량입력 >> ");
+        return scanner.nextInt();
     }
 }

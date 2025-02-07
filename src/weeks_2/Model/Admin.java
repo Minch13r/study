05cf4c31a6eef433431bf041d1582559dc6373fe
3333c <0414minyoung@naver.com>
2025-02-07 19:48:47 +0900

    MVC Merge

--- src/weeks_2/Model/Admin.java
+++ src/weeks_2/Model/Admin.java
@@ -5,25 +5,11 @@ import weeks_2.View.ClientView;
 import weeks_2.View.View;
 
 public class Admin {
-    private MovieDAO movieDao;
-    private MemberDAO memberDao;
-    private MemberDTO memberDTO;
-    private MovieDTO movieDTO;
-    private AdminView adminView;
-    private ClientView clientView;
-    private View baseView;
-    private Crawling crawling;
+
     private MemberDTO admin; // admin 객체를 필드로 선언
 
 
     public Admin() {
-        this.movieDao = new MovieDAO();
-        this.memberDao = new MemberDAO();
-        this.adminView = new AdminView();
-        this.clientView = new ClientView();
-        this.baseView = new View();
-        this.memberDTO = new MemberDTO();
-        this.movieDTO = new MovieDTO();
         this.admin = new MemberDTO();
         this.admin.setId("윤지팀");
         this.admin.setPw("9999");

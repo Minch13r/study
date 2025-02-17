a78ae56e8f2dbc42b3c36ebd9e27d10a3a9797dc
Minch13r <0414minyoung@naver.com>
2025-02-17 14:45:15 +0900

    View 기초설계

---
+++ src/day026/view/View.java
@@ -0,0 +1,29 @@
+package day026.view;
+
+import org.openqa.selenium.json.JsonOutput;
+
+import java.util.Scanner;
+
+public class View {
+    private Scanner sc;
+    public View() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public void printMenuLogin(){
+        System.out.println("1. 이름변경");
+        System.out.println("2. 회원탈퇴");
+        System.out.println("3. 글 작성");
+        System.out.println("4. 글 선택");
+        System.out.println("5. 글 삭제");
+        System.out.println("6. 로그아웃");
+    }
+    public void printMenuLogOut(){
+        System.out.println("7. 회원가입");
+        System.out.println("8. 로그인");
+        System.out.println("9. 목록출력");
+        System.out.println("10. 검색");
+        System.out.println("11. 프로그램 종료");
+    }
+
+}

d615fe142f44ec090808ddac0661e89f6710b15b
Minch13r <0414minyoung@naver.com>
2025-02-17 15:46:42 +0900

    jdbc 연결

--- src/day026/model/MemberDAO.java
+++ src/day026/model/MemberDAO.java
@@ -1,5 +1,9 @@
 package day026.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MemberDAO {
@@ -17,7 +21,44 @@ public class MemberDAO {
         * 저장 잘되면 T
         * 저장 안되면 F
         * */
-        return false;
+        // [ JDBC ], Java DataBase Connection
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            // 1. 드라이버 로드(메모리에 데이터 적재)
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // 2. DB 연결
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/root", "root", "12345678");
+            // 3. 데이터 read, write
+            pstmt = conn.prepareStatement("INSERT INTO MEMBER VALUES (?, ?, ?)"); // parsing
+            pstmt.setString(1, dto.getMember_id());
+            pstmt.setString(2, dto.getMember_password());
+            pstmt.setString(3, dto.getMember_name());
+            int result = pstmt.executeUpdate();
+
+
+            if (result <= 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            // 4. DB 연결 해제
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
     public boolean update(MemberDTO dto){
         // 이름변경

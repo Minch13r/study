a504c3520ef07e3ee418a290306a73b919c938a1
Minch13r <0414minyoung@naver.com>
2025-02-17 14:45:07 +0900

    기능 기초 설정 및 설계

---
+++ src/day026/model/BoardDAO.java
@@ -0,0 +1,27 @@
+package day026.model;
+
+import java.util.ArrayList;
+
+public class BoardDAO {
+    private ArrayList<BoardDAO> selectAll(BoardDAO dto){
+        // 목록출력
+        // 검색
+        return null;
+    }
+    public BoardDAO selectOne(BoardDAO dto){
+        // 선택
+        return null;
+    }
+    public boolean insert(BoardDAO dto){
+        // 작성
+        return false;
+    }
+    public boolean update(BoardDAO dto){
+        // 내용변경
+        return false;
+    }
+    public boolean delete(BoardDAO dto){
+        // 삭제
+        return false;
+    }
+}
---
+++ src/day026/model/MemberDAO.java
@@ -0,0 +1,25 @@
+package day026.model;
+
+import java.util.ArrayList;
+
+public class MemberDAO {
+    private ArrayList<MemberDTO> selectAll(MemberDTO dto){
+        return null;
+    }
+    public MemberDTO selectOne(MemberDTO dto){
+        // 로그인
+        return null;
+    }
+    public boolean insert(MemberDTO dto){
+        // 회원가입
+        return false;
+    }
+    public boolean update(MemberDTO dto){
+        // 이름변경
+        return false;
+    }
+    public boolean delete(MemberDTO dto){
+        // 회원탈퇴
+        return false;
+    }
+}

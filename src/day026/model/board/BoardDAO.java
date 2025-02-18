b9ce6acea1580059704c02ef66803d62397f1d1f
Minch13r <0414minyoung@naver.com>
2025-02-18 11:02:28 +0900

    글 전체 출력 기능 추가

--- src/day026/model/board/BoardDAO.java
+++ src/day026/model/board/BoardDAO.java
@@ -43,33 +43,6 @@ public class BoardDAO {
         }
         return datas;
     }
-    /*
-    * public ArrayList<BoardDTO> selectAll(){
-        ArrayList<BoardDTO> datas = new ArrayList<>();
-        try {
-            conn = JDBCUtil.connect();
-            pstmt = conn.prepareStatement(SELECTALL);  // SELECTALL 쿼리 사용
-            rs = pstmt.executeQuery();
-
-            while(rs.next()) {
-                BoardDTO boardDTO = new BoardDTO();
-                boardDTO.setNum(rs.getInt("NUM"));
-                boardDTO.setTitle(rs.getString("TITLE"));
-                boardDTO.setContent(rs.getString("CONTENT"));
-                boardDTO.setWriter(rs.getString("WRITER"));
-                boardDTO.setCnt(rs.getInt("CNT"));
-                boardDTO.setRegdate(rs.getTimestamp("REGDATE"));
-                datas.add(boardDTO);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            JDBCUtil.disconnect(conn, pstmt, rs);  // ResultSet도 닫아주어야 합니다
-        }
-        return datas;  // 데이터 반환
-    }
-}
-    * */
 
     // 선택
     public BoardDAO selectOne(BoardDTO dto){
